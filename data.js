137f4b3fbc56cbb980b485b5fe1d6b83830201a2
alikaraa <alikaraa61@outlook.com>
2021-03-12 14:41:23 +0300

    RestApi

---
+++ data.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: 1,
+    isim: "Kemal Sunal",
+    filmler: ["Sakar Sakir", "Uc Kagitci", "Tosun Pasa"],
+  },
+  {
+    id: 2,
+    isim: "Sener Sen",
+    filmler: ["Davaro", "Kibar Feyzo", "Tosun Pasa"],
+  },
+  {
+    id: 3,
+    isim: "Adile Nasit",
+    filmler: ["Davaro", "Neseli Gunler", "Sut Kardesler"],
+  },
+  {
+    id: 4,
+    isim : "Angeline Jolie",
+    filmler: ["Hayatim Benim", "Wanted", "60 Saniye"],
+},
+{
+    id: 5,
+    isim : "Vin Diesel",
+    filmler: ["Hizli ve Ofkeli 8", "Ajan XXX", "Hizli ve Ofkeli 7"],
+},
+{
+    id: 6,
+    isim : "Will Smith",
+    filmler: ["I am Legend", "Focus", "Siyah Giyen Adamlar"],
+},
+];
